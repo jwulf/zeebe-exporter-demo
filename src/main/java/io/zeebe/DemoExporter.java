91870341cc554b0fbb0f626fa8403a4aa17e9878
Josh Wulf <josh@magikcraft.io>
2020-03-04 22:21:46 +1000

    Update DemoExporter.java

--- src/main/java/io/zeebe/DemoExporter.java
+++ src/main/java/io/zeebe/DemoExporter.java
@@ -2,7 +2,7 @@ package io.zeebe;
 
 import io.zeebe.exporter.api.context.Controller;
 import io.zeebe.exporter.api.record.Record;
-import io.zeebe.exporter.api.spi.Exporter;
+import io.zeebe.exporter.api.Exporter;
 
 public class DemoExporter implements Exporter {
     Controller controller;
